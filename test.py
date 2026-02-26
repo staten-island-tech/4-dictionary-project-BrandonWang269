d7c974702436f6f67d8524b7799b5a921dc79e56
BrandonWang269 <brandonw269@nycstudents.net>
2026-02-26 10:12:21 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+""" item = {
+    'name': 'iPhone Pro Max 17',
+    'price': '$1099',
+    'department': 'Phones'
+}
+
+print(item['name']) """
+
+
+
+
+best_buy_items =[
+{
+    'name': 'iPhone Pro Max 17',
+    'price': '$1099',
+    'department': 'Phones'
+},
+
+{
+    'name': 'Samsung Galaxy s27',
+    'price': '$799'
+    'department': 'Phone'
+}
+]
+
+print(best_buy_items[1]['name'])
