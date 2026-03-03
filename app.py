23b3bbb4446b34884474ffadf4b863496a3d5cf0
BrandonWang269 <brandonw269@nycstudents.net>
2026-03-03 10:17:21 -0500

    Magnus when I'm dying

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
     print(found)
     occupied(5, "CC.CC", ".CC..") """
 
-def language(sent):
+""" def language(sent):
     t = 0
     s = 0
     for i in sent:
@@ -18,4 +18,28 @@ def language(sent):
         print('French')
     else:
         print('English')
-language('The red cat sat on the mat.')
+language('The red cat sat on the mat.') """
+
+def croation(COCI):
+    search = 1
+    H == 1
+    O == 2
+    N == 3
+    I == 4
+    for i in COCI:
+        if i == 'H':
+            H +=1
+            search = 2
+        elif i == "O":
+            O +=1
+            search = 3
+        elif i == 'N':
+            N +=1
+            search = 4
+        elif i == 'I':
+            I +=1
+            search +=1
+            search = 1
+        if search + 1:
+         print('search')
+        croation('PROHODNIHODNIK')
