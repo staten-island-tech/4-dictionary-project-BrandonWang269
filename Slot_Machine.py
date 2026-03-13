4e724328d72c14ca01c4fc2935858328a8a8998b
BrandonWang269 <brandonw269@nycstudents.net>
2026-03-13 10:10:32 -0400

    Finished

--- Slot_Machine.py
+++ Slot_Machine.py
@@ -1,26 +1,26 @@
-def gambling(total, a, b, c):
-    plays = 0
-    machine = 0
-    while total > 0:
+total = int(input(""))
+c = int(input(""))
+b = int(input(""))
+a = int(input(""))
+plays = 0
+machine = 0
+while total > 0:
         plays += 1
         total -= 1
+        machine = (plays - 1) % 3
         if machine == 0:
             a+=1
             if a == 35:
                 total +=30
                 a=0
-                machine +=1
         if machine == 1:
             b+=1
             if b == 100:
                 total+=60
                 b=0
-                machine +=1
-        if machine == 2:
+        if machine == 2:    
             c+=1
             if c == 10:
                 total +=9
                 c=0
-                machine -=2
-        print(f"Martha plays {plays} times before going broke")
-gambling(48, 3, 10, 4)
+print(f"Martha plays {plays} times before going broke")
